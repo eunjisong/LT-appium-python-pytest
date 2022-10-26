f14fd5ed3700995e0f4efe05c62cf9c66b1d141e
Eunji Song <eunjisong@stride-n99p3cwjc7.lan>
2022-10-26 17:22:34 -0400

    update

--- conftest.py
+++ conftest.py
@@ -2,33 +2,45 @@ from os import environ
 import pytest
 from appium import webdriver
 
+
 @pytest.fixture(scope='function')
 def test_setup_android(request):
     test_name = request.node.name
-    build = environ.get('BUILD', "Pytest Android Sample")
-    caps = {}
-    caps["deviceName"] = "Galaxy S21 5G"
-    caps["platformName"] = "Android"
-    caps["platformVersion"] = "11"
-    caps["app"] = "lt://proverbial-android"   #Enter the app (.apk) url here
-    caps["isRealMobile"] = True
-    caps['build'] = build
-    caps['name'] = test_name
-    caps['project'] = "Android Pytest"
-    driver = webdriver.Remote("https://<username>:<accessKey>@mobile-hub.lambdatest.com/wd/hub", caps)  #Add LambdaTest username and accessKey here
+    build = environ.get('BUILD', "Stride-Android-dev-debug")
+    capability = {
+        "platformName": "android",
+        "deviceName": "Galaxy S21+ 5G",
+        "platformVersion": "11",
+        "app": "lt://APP10160521021666811020909545",
+        "devicelog": True,
+        "visual": True,
+        "network": True,
+        "video": True,
+        "project": "Android Pytest",
+        "deviceOrientation": "portrait",
+        "isRealMobile": True,
+        "build": build,
+        "name": test_name
+    }
+
+    driver = webdriver.Remote(
+        command_executor="https://eunji.song:vxGnlMcez526GZMjS61iJR1LoEvChmEW1vy88jvD3R5vpMPVbL@mobile-hub.lambdatest.com/wd/hub",
+        desired_capabilities=capability)  # Add LambdaTest username and accessKey here
     request.cls.driver = driver
-    
+
     yield driver
-    
+
     def fin():
-        #browser.execute_script("lambda-status=".format(str(not request.node.rep_call.failed if "passed" else "failed").lower()))
+        # browser.execute_script("lambda-status=".format(str(not request.node.rep_call.failed if "passed" else "failed").lower()))
         if request.node.rep_call.failed:
             driver.execute_script('lambda-status=failed')
         else:
             driver.execute_script('lambda-status=passed')
         driver.quit()
+
     request.addfinalizer(fin)
-    
+
+
 @pytest.hookimpl(tryfirst=True, hookwrapper=True)
 def pytest_runtest_makereport(item, call):
     # this sets the result as a test attribute for LambdaTest reporting.
--- tests/test.py
+++ tests/test.py
@@ -1,16 +1,18 @@
 import pytest
+from appium.webdriver.common.appiumby import AppiumBy
+
 
 @pytest.mark.usefixtures('test_setup_android')
 class TestLink:
 
     def test_1(self):
-        el1 = self.driver.find_element_by_id("com.lambdatest.proverbial:id/color")
+        el1 = self.driver.find_elements(AppiumBy.ID, 'R.id.button_primary')
         el1.click()
-        
+
     def test_2(self):
-        el2 = self.driver.find_element_by_id("com.lambdatest.proverbial:id/colour")
+        el2 = self.driver.find_element_by_id("com.stridehealth.drive:id/button_primary")
         el2.click()
-    
+
 
 
 
